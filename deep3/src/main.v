226566927dc201bb72836f527e3416d5b5f222a6
Otso Jousimaa <otso@ojousima.net>
2016-02-18 21:10:14 +0200

    Add exercise 3.5: larson scanner

--- deep3/src/main.v
+++ deep3/src/main.v
@@ -1,6 +1,6 @@
 ---- main.v -------------------------------------------
 
-// Verilog harjoitus v3.4
+// Verilog harjoitus v3.5 - Larson scanner
 // https://kirjoitusalusta.fi/hacklab-x-digi-perus-harjoituksia
 
 `timescale 1ns / 1ps
@@ -13,15 +13,16 @@ module main(
         output  reg     [7:0]       sw_out
     );
 
-    reg         [7:0]   state_next;
-    reg         [7:0]   state = 0;
+    reg         [3:0]   state_next, state = 0;
 
     // Next state kombinaatiologiikka
     always @* begin
-        if (state == 9) begin
+        state_next = state + 1;
+        if (state == 13) begin
             state_next = 0;
-        end else begin
-            state_next = state + 1;
+        end 
+        if (~sw_in[0] == 1'b0) begin
+            state_next = state;
         end
     end
     
@@ -32,7 +33,23 @@ module main(
 
     // Output kombinaatiologiikka
     always @* begin
-        sw_out = state;
-    end
+        sw_out = 8'b00000000;
+        case (state)
+            0:  sw_out = 8'b00000001;
+            1:  sw_out = 8'b00000010;
+            2:  sw_out = 8'b00000100;
+            3:  sw_out = 8'b00001000;
+            4:  sw_out = 8'b00010000;
+            5:  sw_out = 8'b00100000;
+            6:  sw_out = 8'b01000000;
+            7:  sw_out = 8'b10000000;
+            8:  sw_out = 8'b01000000;
+            9:  sw_out = 8'b00100000;
+            10: sw_out = 8'b00010000;
+            11: sw_out = 8'b00001000;
+            12: sw_out = 8'b00000100;
+            13: sw_out = 8'b00000010;
+        endcase
+     end
     
 endmodule
